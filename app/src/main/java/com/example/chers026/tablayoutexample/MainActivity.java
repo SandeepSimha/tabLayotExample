90951942e9e643ea5aba7ade5f36a0abaccca833
Ramesh <ramesh.kec.85@gmail.com>
2017-04-14 00:51:48 +0530

    Custom View for Tabs.

--- app/src/main/java/com/example/chers026/tablayoutexample/MainActivity.java
+++ app/src/main/java/com/example/chers026/tablayoutexample/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.chers026.tablayoutexample;
 
 import android.content.Context;
 import android.graphics.Typeface;
+import android.support.annotation.DrawableRes;
+import android.support.annotation.StringRes;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -12,8 +14,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.AppCompatTextView;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -21,6 +25,7 @@ import java.util.List;
 import java.util.logging.Logger;
 
 public class MainActivity extends AppCompatActivity {
+    TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,59 +37,70 @@ public class MainActivity extends AppCompatActivity {
         viewPager.setAdapter(new SampleFragmentPagerAdapter(getSupportFragmentManager(), MainActivity.this));
 
         // Give the TabLayout the ViewPager
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
+        tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
         tabLayout.setupWithViewPager(viewPager);
 
         final ViewGroup vg = (ViewGroup) tabLayout.getChildAt(0);
         final int tabsCount = vg.getChildCount();
-        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-            @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-                Log.e("Tab count", "Unselected = " + tab.getPosition());
-
-                ViewGroup vgTab = (ViewGroup) vg.getChildAt(tab.getPosition());
-                int tabChildsCount = vgTab.getChildCount();
-
-                for (int i = 0; i < tabChildsCount; i++) {
-                    View tabViewChild = vgTab.getChildAt(i);
-                    if (tabViewChild instanceof AppCompatTextView) {
-                        Log.e("Tab count", "Infont");
-                        Typeface type = Typeface.createFromAsset(getApplicationContext().getAssets(), "AvenirLTStd-Heavy.otf");
-                        TextView viewChild = (TextView) tabViewChild;
-                        viewChild.setTypeface(type);
-                        viewChild.setAllCaps(false);
-                    }
-                }
-            }
-
-            @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
-                Log.e("Tab count", "selected = " + tab.getPosition());
-
-                ViewGroup vgTab = (ViewGroup) vg.getChildAt(tab.getPosition());
-                int tabChildsCount = vgTab.getChildCount();
-
-                for (int i = 0; i < tabChildsCount; i++) {
-                    View tabViewChild = vgTab.getChildAt(i);
-                    if (tabViewChild instanceof AppCompatTextView) {
-                        Log.e("Tab count", "Infont");
-                        Typeface type = Typeface.createFromAsset(getApplicationContext().getAssets(), "AvenirLTStd-Roman.otf");
-                        TextView viewChild = (TextView) tabViewChild;
-                        viewChild.setTypeface(type);
-                        viewChild.setAllCaps(false);
-                    }
-                }
-            }
-
-            @Override
-            public void onTabReselected(TabLayout.Tab tab) {
-                //onTabSelected(tab);
-            }
-        });
+//        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+//            @Override
+//            public void onTabSelected(TabLayout.Tab tab) {
+//                Log.e("Tab count", "Unselected = " + tab.getPosition());
+//
+//                ViewGroup vgTab = (ViewGroup) vg.getChildAt(tab.getPosition());
+//                int tabChildsCount = vgTab.getChildCount();
+//
+//                for (int i = 0; i < tabChildsCount; i++) {
+//                    View tabViewChild = vgTab.getChildAt(i);
+//                    if (tabViewChild instanceof AppCompatTextView) {
+//                        Log.e("Tab count", "Infont");
+//                        Typeface type = Typeface.createFromAsset(getApplicationContext().getAssets(), "AvenirLTStd-Heavy.otf");
+//                        TextView viewChild = (TextView) tabViewChild;
+//                        viewChild.setTypeface(type);
+//                        viewChild.setAllCaps(false);
+//                    }
+//                }
+//            }
+//
+//            @Override
+//            public void onTabUnselected(TabLayout.Tab tab) {
+//                Log.e("Tab count", "selected = " + tab.getPosition());
+//
+//                ViewGroup vgTab = (ViewGroup) vg.getChildAt(tab.getPosition());
+//                int tabChildsCount = vgTab.getChildCount();
+//
+//                for (int i = 0; i < tabChildsCount; i++) {
+//                    View tabViewChild = vgTab.getChildAt(i);
+//                    if (tabViewChild instanceof AppCompatTextView) {
+//                        Log.e("Tab count", "Infont");
+//                        Typeface type = Typeface.createFromAsset(getApplicationContext().getAssets(), "AvenirLTStd-Roman.otf");
+//                        TextView viewChild = (TextView) tabViewChild;
+//                        viewChild.setTypeface(type);
+//                        viewChild.setAllCaps(false);
+//                    }
+//                }
+//            }
+//
+//            @Override
+//            public void onTabReselected(TabLayout.Tab tab) {
+//                //onTabSelected(tab);
+//            }
+//        });
 
         Log.e("Tab count", "Tabcount = " + tabsCount);
 
+        addCustomLayout(0, 0, R.string.tab1);
+        addCustomLayout(1, 0, R.string.tab2);
+        addCustomLayout(2, 0, R.string.tab3);
     }
+
+    private void addCustomLayout(final int position, @DrawableRes int drawableId, @StringRes int resId) {
+        View v = LayoutInflater.from(this).inflate(R.layout.layout_tab_title, null);
+        TextView tabTitle = (TextView) v.findViewById(R.id.txt_title);
+        tabTitle.setText(resId);
+        tabLayout.getTabAt(position).setCustomView(v);
+    }
+
 
 }
 
